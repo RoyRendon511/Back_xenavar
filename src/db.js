4e0d802d3ae500f26f83aad35767acd05e6651e6
Franco <ti43243@uvp.edu.mx>
2024-06-10 01:53:18 -0600

    arreglando back9

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.DB_DATABASE, {
+    await mongoose.connect(process.env.MONGODB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       serverSelectionTimeoutMS: 10000,
