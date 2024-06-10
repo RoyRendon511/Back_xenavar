54c1e5f613e5dd70e52e012f8433c3bca07addbe
Franco <ti43243@uvp.edu.mx>
2024-06-09 23:13:47 -0600

    arreglando back

--- src/app.js
+++ src/app.js
@@ -8,16 +8,17 @@ import productoRoutes from './routes/productos.routes.js';
 const app = express();
 
 app.use(cors({ 
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost:5173', // Cambiar esto a la URL de tu frontend en producción
     credentials: true 
 })); 
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static('public'));
-app.use("/api",authRoutes);
-app.use("/api",productoRoutes);
-app.use((req,res) => {
-    res.status(404).json({status:false, message: "Ruta no encontrada"});
-})
+app.use("/api", authRoutes);
+app.use("/api", productoRoutes);
+app.use((req, res) => {
+    res.status(404).json({ status: false, message: "Ruta no encontrada" });
+});
+
 export default app;
--- src/db.js
+++ src/db.js
@@ -1,9 +1,33 @@
 import mongoose from "mongoose";
+
 export const connectDB = async () => {
-    try {
-        await mongoose.connect('mongodb+srv://ti43243:Francost15@cluster0.gonyppz.mongodb.net/merndb?retryWrites=true&w=majority&appName=Cluster0')
-        console.log("Connected to MongoDB Atlas")
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    await mongoose.connect('mongodb+srv://ti43243:Francost15@cluster0.gonyppz.mongodb.net/merndb?retryWrites=true&w=majority&appName=Cluster0', {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // Esperar hasta 10 segundos para conectarse
+      socketTimeoutMS: 45000, // Tiempo de espera de socket
+    });
+
+    mongoose.connection.on('connected', () => {
+      console.log('Mongoose connected to DB Cluster');
+    });
+
+    mongoose.connection.on('error', (err) => {
+      console.log(err.message);
+    });
+
+    mongoose.connection.on('disconnected', () => {
+      console.log('Mongoose Disconnected');
+    });
+
+    process.on('SIGINT', async () => {
+      await mongoose.connection.close();
+      process.exit(0);
+    });
+
+    console.log("Connected to MongoDB Atlas");
+  } catch (error) {
+    console.log("Could not connect to MongoDB Atlas: ", error);
+  }
+};
